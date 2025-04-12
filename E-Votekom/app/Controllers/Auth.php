832b963c568bd964b3259dbe9440c192d6a1e516
Banu Tasila <banufadel83@gmail.com>
2025-04-12 13:32:01 +0800

    wea

--- E-Votekom/app/Controllers/Auth.php
+++ E-Votekom/app/Controllers/Auth.php
@@ -6,6 +6,6 @@ class Auth extends BaseController
 {
     public function login()
     {
-        return view('auth/login'); // arahkan ke view login
+        return view('Auth/login'); // arahkan ke view login
     }
 }
