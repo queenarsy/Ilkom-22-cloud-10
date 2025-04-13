b5ffcda1bf7f82c0e3e99f66e2c31b44ebef34d6
marsandy123 <marsandy.alimran@gmail.com>
2025-04-13 18:01:56 +0800

    klp

--- E-Votekom/app/Views/admin/index.php
+++ E-Votekom/app/Views/admin/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ini halaman admin</title>
+    <title>ini halaman admin klp</title>
 </head>
 <body>
     
