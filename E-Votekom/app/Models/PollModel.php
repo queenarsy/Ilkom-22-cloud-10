74db2a8445be5ccf84c4e14c6b2a162b76352768
bhtrwjya <wijayabahtiar06@gmail.com>
2025-06-30 17:18:48 +0800

    evotecom

--- E-Votekom/app/Models/PollModel.php
+++ E-Votekom/app/Models/PollModel.php
@@ -20,7 +20,7 @@ class PollModel extends Model
         return $this->find($id); //Fungsi cara
     }
 
-    public function createPoll($data) //Fungsi buat data
+    public function createPoll($data) //fungsi buat data
     {
         return $this->insert($data); //fungsi menambah data
     }
