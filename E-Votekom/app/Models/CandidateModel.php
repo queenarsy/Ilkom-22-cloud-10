38ebfb6cc71e4ecb602c1e0271ccf0824b6835b7
bhtrwjya <wijayabahtiar06@gmail.com>
2025-06-30 17:25:17 +0800

    EVOTECOM

--- E-Votekom/app/Models/CandidateModel.php
+++ E-Votekom/app/Models/CandidateModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class CandidateModel extends Model
 {
-    protected $table = 'kadidat'; // ganti nama tabelmu
+    protected $table = 'kadidat'; // Ganti nama tabelmu
     protected $primaryKey = 'kadidat_id'; //kandidat ID
     protected $allowedFields = ['nama', 'bio', 'photo', 'vote'];
 }
