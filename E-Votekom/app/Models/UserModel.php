d700c825178bfb7a2dd6f91e1bd7ff7de8cc781a
bhtrwjya <wijayabahtiar06@gmail.com>
2025-06-30 17:21:15 +0800

    EVOTECOM

--- E-Votekom/app/Models/UserModel.php
+++ E-Votekom/app/Models/UserModel.php
@@ -12,7 +12,7 @@ class UserModel extends Model
 
     public function getUser ($username)
     {
-        return $this->where('username', $username)->first(); //cari username
+        return $this->where('username', $username)->first(); //Cari username
     }
     
    
